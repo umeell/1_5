a46bf6993c240cfdbc7d1776156f1335e7dbcd33
Nikita <baynik01@gmail.com>
2022-03-22 12:22:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int parzysta=0;
+      for(int i=50;parzysta<=i;parzysta++)
+        {
+          if(parzysta%2==0)
+            System.out.println(parzysta);
+        }
+    parzysta=50;
+      for(int i=100;parzysta<=i;parzysta++)
+        {
+          if(parzysta%2 != 0)
+            System.out.println(parzysta);
+        }
+    }
+  }
